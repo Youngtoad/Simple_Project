887859916c4a4a387ace836532aaf4c379153cf2
young <youngjin033@ajou.ac.kr>
2020-01-03 15:25:05 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const db = require("./queries");
+const PORT = process.env.PORT || 4000;
+const proxy = require("http-proxy-middleware");
+
+module.exports = function(app) {
+  app.use(
+    proxy("/users", {
+      target: "http://localhost:4000/"
+    })
+  );
+};
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`);
+});
